82df9c52fdb45df33da17b6b7169828d720905d9
hapo-hungnx <hugnnx@haposoft.com>
2020-10-02 21:26:53 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                             <a class="dropdown-item" href="#">Action 1</a>
                             <a class="dropdown-item" href="#">Action 2</a>
                         </div>
-                    </li>
+                    {{-- </li> --}}
                     <li class="nav-item dropdown d-md-inline-block">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                         <div class="dropdown-menu" aria-labelledby="dropdownId">
